cefc24d9e5ee85fe9772f885756ee87de6aa9fb7
Ali <plewffy@gmail.com>
2023-11-02 14:02:08 +0600

    convex provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,10 @@
-import { ClerkProvider, useAuth } from "@clerk/nextjs";
+import { ClerkProvider } from "@clerk/nextjs";
 
 import type { Metadata } from "next";
 
 import { Inter } from "next/font/google";
 import "./globals.css";
-
 import ConvexClientProvider from "@/components/providers/convex-client-provider";
-import { ConvexProviderWithClerk } from "convex/react-clerk";
-import { ConvexReactClient } from "convex/react";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -34,20 +31,18 @@ export const metadata: Metadata = {
   }
 }
 
-const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL as string);
-
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
+    <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!}>
       <html lang="en">
         <body className={inter.className}>
-          <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+          <ConvexClientProvider>
             {children}
-          </ConvexProviderWithClerk>
+          </ConvexClientProvider>
         </body>
       </html>
     </ClerkProvider>
