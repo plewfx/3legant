57d6a5352a8e03a307dbbfb762172e5fd649f728
Ali <plewffy@gmail.com>
2023-11-02 16:43:01 +0600

    add middleware

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,7 @@
-import { authMiddleware, redirectToSignIn } from '@clerk/nextjs';
+import { authMiddleware } from "@clerk/nextjs";
  
-export default authMiddleware({
-  afterAuth(auth, req, evt) {
-    // handle users who aren't authenticated
-    if (!auth.userId && !auth.isPublicRoute) {
-      return redirectToSignIn({ returnBackUrl: req.url });
-    }
-    // redirect them to organization selection page
-    if(auth.userId && !auth.orgId && req.nextUrl.pathname !== "/org-selection"){
-      const orgSelection = new URL('/org-selection', req.url)
-      return Response.redirect(orgSelection)
-    }
-  }
-});
+export default authMiddleware();
+ 
+export const config = {
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+};
